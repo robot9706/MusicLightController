d13198b16d348df9db7800fdc6db0244b3c770aa
Nagygyörgy Bence <robot9706@gmail.com>
2016-09-16 20:10:42 +0200

    ATmega328P-PU sketch included

---
+++ MusicLightController/Misc/serial_pwm.ino
@@ -0,0 +1,21 @@
+int pwm = 5;
+int pwm2 = 6;
+
+void setup() {
+  pinMode(pwm, OUTPUT);
+  analogWrite(pwm, 255);
+
+  pinMode(pwm2, OUTPUT);
+  analogWrite(pwm2, 255);
+
+  Serial.begin(115200);
+}
+
+int val = 0;
+
+void loop() {
+  if(Serial.available() > 1){
+    analogWrite(pwm, Serial.read());
+    analogWrite(pwm2, Serial.read());
+  }
+}
